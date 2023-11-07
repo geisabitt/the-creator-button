4d7d0227df2a48e5ecf327eeceb8867d650115e4
geisabitt <ageisislane@gmail.com>
2023-11-07 13:42:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const controles = document.getElementById("controles");
+const cssText = document.querySelector(".css");
+const btn = document.querySelector(".btn");
+
+controles.addEventListener("change", handleChange);
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + "px";
+  },
+  width(value) {
+    this.element.style.width = value + "px";
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + "px";
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + "rem";
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+};
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+  saveValues(name, value);
+  showCss();
+
+  console.log(name, value);
+}
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach((propertie) => {
+    controles.elements[propertie].value = localStorage[propertie];
+    handleStyle[propertie](localStorage[propertie]);
+    console.log(controles.elements[propertie]);
+  });
+  showCss();
+
+  console.log(properties);
+}
+
+setValues();
+function showCss() {
+  cssText.innerHTML = `<span>${btn.style.cssText
+    .split("; ")
+    .join(";</span><span>")}`;
+}
